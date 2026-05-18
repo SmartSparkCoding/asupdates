2e621ceac9d852d14e02c82b450b292d7a4f9279
Jacob Navaratne <jacob.navaratne@outlook.com>
2026-05-18 18:56:31 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
-SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key-change-me")
+SECRET_KEY = os.getenv("SECRET_KEY", "dev-key-change")
 ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD", "admin123")
 
 DATABASE = "data.db"
 
-EMAIL_FROM = os.getenv("EMAIL_FROM", "")
-EMAIL_APP_PASSWORD = os.getenv("EMAIL_APP_PASSWORD", "")
+EMAIL_FROM = os.getenv("EMAIL_FROM")
+EMAIL_APP_PASSWORD = os.getenv("EMAIL_APP_PASSWORD")
