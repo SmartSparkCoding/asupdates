6d90f2386b200ecf7a2545e96e5f85989158627d
Jacob Navaratne <jacob.navaratne@outlook.com>
2026-05-18 18:56:44 +0100

    Update db.py

--- db.py
+++ db.py
@@ -1,31 +1,31 @@
 import sqlite3
 from config import DATABASE
 
-
 def get_db():
-    return sqlite3.connect(DATABASE)
-
+    conn = sqlite3.connect(DATABASE)
+    conn.row_factory = sqlite3.Row
+    return conn
 
 def init_db():
     db = get_db()
     c = db.cursor()
 
+    # USERS
     c.execute("""
     CREATE TABLE IF NOT EXISTS users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         email TEXT UNIQUE,
         pin TEXT,
-        send_emails INTEGER DEFAULT 1,
-        week_group TEXT DEFAULT 'A'
+        send_emails INTEGER DEFAULT 1
     )
     """)
 
+    # SETTINGS
     c.execute("""
     CREATE TABLE IF NOT EXISTS settings (
         id INTEGER PRIMARY KEY,
-        week_type TEXT DEFAULT 'A',
         holiday_mode INTEGER DEFAULT 0,
-        custom_message TEXT DEFAULT ''
+        week_type TEXT DEFAULT 'A'
     )
     """)
 
